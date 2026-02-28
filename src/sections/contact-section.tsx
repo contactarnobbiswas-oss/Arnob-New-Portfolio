00ebe32a1014a49867cdb45c05dc6b76ad6916d2
contactarnobbiswas-oss <contact.arnobbiswas@gmail.com>
2026-03-01 00:36:54 +0600

    Update contact-section.tsx

--- src/sections/contact-section.tsx
+++ src/sections/contact-section.tsx
@@ -3,15 +3,16 @@ import Section from "../components/section";
 export default function ContactSection() {
     return (
         <Section title="Contact Me">
-            <table className="table-auto mr-auto">
+            {/* এখানে text-base ক্লাসটি যোগ করে পুরো টেবিলের লেখা ১ সাইজ বড় করা হয়েছে */}
+            <table className="table-auto mr-auto text-base">
                 <tbody>
                     <tr>
                         <td className="pr-4 py-2">Email:</td>
-                        <td className="py-2 text-gray-500">contact@prebuiltui.com</td>
+                        <td className="py-2 text-gray-500">contact.arnobbiswas@gmail.com</td>
                     </tr>
                     <tr>
                         <td className="pr-4 py-2">Phone:</td>
-                        <td className="py-2 text-gray-500">+1 (555) 555-5555</td>
+                        <td className="py-2 text-gray-500">+8801806581267</td>
                     </tr>
                 </tbody>
             </table>
