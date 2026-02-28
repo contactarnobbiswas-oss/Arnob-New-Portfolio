e332b3b856ab95e20c77911c5794e40deb033268
contactarnobbiswas-oss <contact.arnobbiswas@gmail.com>
2026-03-01 00:16:35 +0600

    Update section.tsx

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -6,7 +6,10 @@ interface Props {
 export default function Section({ title, children }: Props) {
     return (
         <section className="flex flex-col md:flex-row items-center justify-center md:items-start gap-8 w-full max-w-4xl mx-auto mt-28">
-            <p className="text-xl text-center md:text-left md:text-lg font-medium pt-3 w-full md:max-w-42">{title}</p>
+            {/* টাইটেল ২ সাইজ বড় করা হয়েছে: text-xl -> text-3xl (Mobile) এবং text-lg -> text-2xl (Desktop) */}
+            <p className="text-3xl text-center md:text-left md:text-2xl font-medium pt-3 w-full md:max-w-42">
+                {title}
+            </p>
             {children}
         </section>
     );
