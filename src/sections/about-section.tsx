1cc39f7ef957ea7d066fbab266746c33b5d212e2
contactarnobbiswas-oss <contact.arnobbiswas@gmail.com>
2026-03-01 00:31:44 +0600

    Update about-section.tsx

--- src/sections/about-section.tsx
+++ src/sections/about-section.tsx
@@ -1,31 +1,17 @@
 import Section from "../components/section";
 
-
-
 export default function AboutSection() {
-
     return (
-
         <Section title="About Me">
-
-            <div className="text-sm/6.5">
-
+            {/* এখানে শুধুমাত্র text-sm/6.5 পরিবর্তন করে text-base/7 করা হয়েছে */}
+            <div className="text-base/7">
                 <p>
-
                     I’m Rayan Walker, a full-stack developer passionate about building end-to-end digital experiences that are fast, scalable and user-focused. With strong expertise in both front-end and back-end technologies, I enjoy transforming ideas into fully functional web applications that feel seamless and intuitive.
-
                 </p>
-
                 <p className="mt-5">
-
                     Whether it’s crafting responsive interfaces or developing robust backend systems, I aim to deliver solutions that are both reliable and impactful.
-
                 </p>
-
             </div>
-
         </Section>
-
     );
-
 }
